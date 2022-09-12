5d2e378851226ad402f6a93b85e223614a15be9f
BohmChad-FS <crbohm@student.fullsail.edu>
2022-09-11 23:06:00 -0400

    OAuth creation

--- api/server.js
+++ api/server.js
@@ -8,6 +8,8 @@ app.use(cors())
 
 const PORT = process.env.PORT || 3001;
 
+// const spotifyRouter = require('./routes/spotify)
+
 const mongoDBURL = process.env.mongoDBURL;
 
 mongoose.connect(mongoDBURL, { useNewUrlParser: true })
@@ -16,6 +18,7 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.log("Database Connection Established"))
 
 app.use(express.json())
+// app.use('/spotify/v0', spotifyRouter)
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
