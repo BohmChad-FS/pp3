77fb417b11863730cb6287d95c0a6fff64c958be
BohmChad-FS <crbohm@student.fullsail.edu>
2022-09-25 22:52:38 -0400

    final commit

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,13 +1,22 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Header from "./components/header"
+import Login from "./pages/loginPage"
+import Home from "./pages/home"
 
 function App() {
   return (
-    <div>
-      <Header />
+    <div style={background}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
+const background = {
+  background: '#191414',
+  height: '100vh'
+}
